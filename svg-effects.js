3307dbf46de640e009913c00127f4a4d0cfcfbf5
flinga <flinga@cs.washington.edu>
2016-10-18 23:01:37 -0700

    Added drop shadow helper
    New file for special effects

---
+++ svg-effects.js
@@ -0,0 +1,19 @@
+// need to use additional CSS to use this, e.g.:
+//    rect:hover {
+//      filter: url("#shadow");
+//    }
+function addDropShadowFilter(svg, id) {
+  var filter = svg.append("filter")
+                  .attr("id", id)
+                  .attr("x", -2)
+                  .attr("y", -2)
+                  .attr("width", 200)
+                  .attr("height", 200);
+  filter.append("feGaussianBlur")
+        .attr("in", "SourceAlpha")
+        .attr("stdDeviation", 4);
+  filter.append("feOffset").attr("dx", 0).attr("dy", 0);
+  var filterMerge = filter.append("feMerge");
+  filterMerge.append("feMergeNode");
+  filterMerge.append("feMergeNode").attr("in", "SourceGraphic");
+}
