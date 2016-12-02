e2c59ed5299da2f768e79e9eb680d6b6aa564bb2
flinga <flinga@cs.washington.edu>
2016-12-02 09:38:45 -0800

    round

--- smalls.js
+++ smalls.js
@@ -22,3 +22,8 @@ function undersToSpaces(str) {
 function parensToUnders(str) {
   return str.replace(/\(|\)/g, "_");
 }
+
+// rounds 'number' to 'decimals' decimal places
+function round(number, decimals) {
+  return Number(Math.round(number + 'e' + decimals) + 'e-' + decimals);
+}
