23412221696c5e1d1c9921f1c6a9520ecc301951
flinga <flinga@cs.washington.edu>
2017-03-05 12:26:39 -0800

    add resizability to SVGContainer

--- html-elements.js
+++ html-elements.js
@@ -6,17 +6,17 @@ class SVGContainer {
 
     constructor (parentId, divClass, svgClass, resizeCallback, margin, height) {
         var me = this;
-
         me.parentId = parentId;
+        me.parent = document.getElementById(me.parentId);
         me.divClass = divClass;
         me.svgClass = svgClass;
         me.resizeCallback = resizeCallback;
         me.margin = margin;
-        me.parent = document.getElementById(me.parentId);
         me.divWidth = me.parent.clientWidth;
         me.divHeight = height;
         me.svgWidth = me.divWidth - me.margin.left - me.margin.right;
         me.svgHeight = me.divHeight - me.margin.top - me.margin.bottom;
+
         me.div = d3.select('#' + me.parentId)
             .append('div')
             .attr('class', divClass);
@@ -26,18 +26,25 @@ class SVGContainer {
         me.svg = me.SVG
             .append('g')
             .attr('transform', 'translate(' + me.margin.left + ',' + me.margin.top + ')');
+
         window.addEventListener('resize', me.resizeCallback);
     }
 
-    resize () {
+    resize (height) {
         var me = this;
-
         me.divWidth = me.parent.clientWidth;
+        me.divHeight = height || me.divHeight;
         me.svgWidth = me.divWidth - me.margin.left - me.margin.right;
+        me.svgHeight = me.divHeight - me.margin.top - me.margin.bottom;
+
         me.SVG
             .attr('width', me.divWidth)
             .attr('height', me.divHeight);
-        return me.svgWidth;
+
+        return {
+            svgWidth: me.svgWidth,
+            svgHeight: me.svgHeight
+        };
     }
 }
 
