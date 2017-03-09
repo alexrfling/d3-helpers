11c1162c9264662b6d2a267264940501497c4b6a
flinga <flinga@cs.washington.edu>
2017-03-08 21:23:12 -0800

    better htmlEscape

--- htmlEscape.js
+++ htmlEscape.js
@@ -1,4 +1,4 @@
 // returns a string that should be usable as an HTML id (work in progress)
 function htmlEscape (str) {
-    return str.replace(/\||\.|\(|\)|'|"| /g, '_');
+    return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
 }
