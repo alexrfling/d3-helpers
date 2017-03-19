456846ea4f00795a654270e99bbccec4ac919f97
flinga <flinga@cs.washington.edu>
2017-03-18 20:21:58 -0700

    initial resize in svgContainer

--- svgContainer.js
+++ svgContainer.js
@@ -1,12 +1,12 @@
 class SVGContainer {
 
-    constructor (parentId, divClass, svgClass, resizeCallback, margin, height) {
+    constructor (parentId, divClass, svgClass, onWindowResize, margin, height) {
         var me = this;
         me.parentId = parentId;
         me.parent = document.getElementById(me.parentId);
         me.divClass = divClass;
         me.svgClass = svgClass;
-        me.resizeCallback = resizeCallback;
+        me.onWindowResize = onWindowResize;
         me.margin = margin;
         me.divWidth = me.parent.clientWidth;
         me.divHeight = height;
@@ -15,21 +15,24 @@ class SVGContainer {
 
         me.div = d3.select('#' + me.parentId)
             .append('div')
-            .attr('class', divClass);
+            .attr('class', me.divClass);
         me.SVG = me.div
             .append('svg')
-            .attr('class', svgClass);
+            .attr('class', me.svgClass);
         me.svg = me.SVG
             .append('g')
             .attr('transform', 'translate(' + me.margin.left + ',' + me.margin.top + ')');
 
-        window.addEventListener('resize', me.resizeCallback);
+        window.addEventListener('resize', me.onWindowResize);
+
+        // initialize
+        me.resize();
     }
 
     resize (height) {
         var me = this;
         me.divWidth = me.parent.clientWidth;
-        me.divHeight = height || me.divHeight;
+        me.divHeight = (height || me.divHeight);
         me.svgWidth = me.divWidth - me.margin.left - me.margin.right;
         me.svgHeight = me.divHeight - me.margin.top - me.margin.bottom;
 
@@ -38,6 +41,8 @@ class SVGContainer {
             .attr('height', me.divHeight);
 
         return {
+            divWidth: me.divWidth,
+            divHeight: me.divHeight,
             svgWidth: me.svgWidth,
             svgHeight: me.svgHeight
         };
