fd07e63bb119c50db8e2fa67c96a0c1f161491ef
flinga <flinga@cs.washington.edu>
2017-03-24 04:54:15 -0700

    move interpolateColors to Widget

--- widget.js
+++ widget.js
@@ -20,6 +20,19 @@ class Widget {
         return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
     }
 
+    interpolateColors (low, mid, high, length) {
+        var lowToMidF = d3.interpolateLab(low, mid);
+        var lowToMid = d3.range(Math.floor(length / 2)).map(function (j) {
+            return lowToMidF(j / Math.floor(length / 2));
+        });
+        var midToHighF = d3.interpolateLab(mid, high);
+        var midToHigh = d3.range(Math.ceil(length / 2)).map(function (j) {
+            return midToHighF(j / Math.ceil(length / 2));
+        });
+
+        return lowToMid.concat(midToHigh);
+    }
+
     // returns the key field of the given object
     key (d) {
         return d.key;
