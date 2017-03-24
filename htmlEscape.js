9a3c7d2ecfb34667f475902215369dc7cd6026b5
flinga <flinga@cs.washington.edu>
2017-03-24 04:45:41 -0700

    move htmlEscape into Widget

--- htmlEscape.js
+++
@@ -1,4 +0,0 @@
-// returns a string that should be usable as an HTML id (work in progress)
-function htmlEscape (str) {
-    return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
-}
--- widget.js
+++ widget.js
@@ -14,4 +14,9 @@ class Widget {
             node.removeChild(node.firstChild);
         }
     }
+
+    // returns a String that should be usable as an HTML id (work in progress)
+    htmlEscape (str) {
+        return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
+    }
 }
