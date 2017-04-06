989ea5fbfec513c3d0f89fd0985cdea16e8e67b9
flinga <flinga@cs.washington.edu>
2017-04-05 22:18:08 -0700

    another comment nit

--- widget.js
+++ widget.js
@@ -15,7 +15,7 @@ class Widget {
         }
     }
 
-    // returns a String that should be usable as an HTML id (work in progress)
+    // returns a string that should be usable as an HTML id (work in progress)
     htmlEscape (str) {
         return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
     }
