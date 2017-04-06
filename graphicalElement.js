03373d6ac45fef0d2659f2e0d5222d3b0586618e
flinga <flinga@cs.washington.edu>
2017-04-05 22:14:49 -0700

    comment nit

--- graphicalElement.js
+++ graphicalElement.js
@@ -22,7 +22,7 @@ class GraphicalElement {
         return me.group._groups[0][0].getBoundingClientRect();
     }
 
-    // returns a String that should be usable as an HTML id (work in progress)
+    // returns a string that should be usable as an HTML id (work in progress)
     htmlEscape (str) {
         return ('_' + str).replace(/\||\.|\(|\)|'|"| /g, '_');
     }
