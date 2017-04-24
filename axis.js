3e30f1347f6797bd325830124c044ace5ca2b31c
flinga <flinga@cs.washington.edu>
2017-04-24 13:38:12 -0700

    add Axis class

---
+++ axis.js
@@ -0,0 +1,37 @@
+class Axis extends GraphicalElement {
+
+    constructor (svg, className, scale, fontSize, orientation, options) {
+        super(svg, className, options);
+
+        var me = this;
+        me.scale = scale;
+        me.fontSize = fontSize;
+
+        me.group
+            .style('font-size', me.fontSize);
+
+        switch (orientation) {
+            case 'top':
+                me.axis = d3.axisTop(me.scale);
+                break;
+            case 'left':
+                me.axis = d3.axisLeft(me.scale);
+                break;
+            case 'right':
+                me.axis = d3.axisRight(me.scale);
+                break;
+            case 'bottom':
+                me.axis = d3.axisBottom(me.scale);
+                break;
+        }
+    }
+
+    updateVis (animDuration) {
+        var me = this;
+
+        me.group
+            .transition()
+            .duration(animDuration)
+            .call(me.axis);
+    }
+}
